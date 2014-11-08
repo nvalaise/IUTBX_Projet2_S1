d25c8bd18f0460e695c1e412e563dba8607176a2
nvalaise <nicolas.valaise@hotmail.fr>
2014-11-08 19:29:37 +0100

    Affichage du pirate

---
+++ joueur.cpp
@@ -0,0 +1,20 @@
+#include "joueur.h"
+
+using namespace std;
+
+/*
+void deplacementPirate(Joueur &joueur, Plateau &plateau)
+{
+    if (jeu.event.type == SDL_MOUSEBUTTONDOWN)
+    {
+        if (jeu.event.button.button == SDL_BUTTON_LEFT)
+        {
+            if ()
+            {
+
+            }
+
+        }
+
+    }
+}*/
---
+++ joueur.h
@@ -0,0 +1,45 @@
+#ifndef JOUEUR_H
+#define JOUEUR_H
+
+/***********************************
+Declaration des includes
+************************************/
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+//declaration de la SDL
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+
+//fichier internes
+#include "gestion.h"
+
+/***********************************
+Declaration des constantes
+************************************/
+
+/***********************************
+Declaration des structures
+************************************/
+struct Joueur
+{
+    const int LARGEUR_PIRATE = 61;
+	const int HAUTEUR_PIRATE = 61;
+
+	SDL_Surface* pirate = chargerImage("piratesSprite2.bmp");
+	SDL_Rect pirates[2];
+
+	int xJ, yJ;
+	int valeurJ;
+	int score;
+};
+
+/***********************************
+Declaration des prototype
+************************************/
+
+//void deplacementPirate(Joueur &joueur, Plateau &plateau);
+
+#endif
